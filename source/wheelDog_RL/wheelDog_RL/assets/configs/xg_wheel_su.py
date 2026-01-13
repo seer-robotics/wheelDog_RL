3ad2e607acbabcbbe5c5e26bc64ad0521a59d8c4
Renda Yang <rendayang2002@163.com>
2026-01-13 13:50:46 +0800

    Initial joint positions.

--- source/wheelDog_RL/wheelDog_RL/assets/configs/xg_wheel_su.py
+++ source/wheelDog_RL/wheelDog_RL/assets/configs/xg_wheel_su.py
@@ -1,30 +1,39 @@
-from omni.isaac.lab.assets import ArticulationCfg as ArticCfg
-from omni.isaac.lab.sim.schemas.schemas_cfg import RigidBodyPropertiesCfg
-from omni.isaac.lab.sim.spawners.from_files.from_files_cfg import UsdFileCfg
-from omni.isaac.lab.utils.assets import ISAACLAB_NUCLEUS_DIR
+import isaaclab.sim as sim_utils
+from isaaclab.assets.articulation import ArticulationCfg
+from isaaclab.actuators import ActuatorNetMLPCfg, DCMotorCfg, ImplicitActuatorCfg
 
-YOUR_ROBOT_CFG = ArticCfg(
-    spawn=UsdFileCfg(
-        usd_path=f"{ISAACLAB_NUCLEUS_DIR}/Robots/YourRobot/your_robot.usd",  # Or local path if not on Nucleus
-        rigid_props=RigidBodyPropertiesCfg(
+YOUR_ROBOT_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        # usd_path=f"{ISAACLAB_NUCLEUS_DIR}/Robots/YourRobot/your_robot.usd",  # Or local path if not on Nucleus
+        usd_path="~/workspace/Projects/USD_Files/xg_wheel_su_USD/xg_wheel_su.usd",
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
             disable_gravity=False,
             retain_accelerations=False,
             linear_damping=0.0,
             angular_damping=0.0,
             max_linear_velocity=1000.0,
             max_angular_velocity=1000.0,
-            max_depenetration_velocity=100.0,
+            max_depenetration_velocity=1.0,
         ),
-        activate_contact_sensors=True,
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False, 
+            solver_position_iteration_count=4, 
+            solver_velocity_iteration_count=0
+        ),
     ),
-    init_state=ArticCfg.InitialStateCfg(
+    init_state=ArticulationCfg.InitialStateCfg(
         pos=(0.0, 0.0, 0.5),  # Initial position (e.g., above ground)
-        rot=(1.0, 0.0, 0.0, 0.0),  # Quaternion orientation
-        joint_pos={},  # Default joint positions if needed
+        joint_pos={
+            ".*_ABAD_JOINT": 0.00,
+            ".*_HIP_JOINT": 0.40,
+            ".*_KNEE_JOINT": -0.80,
+            ".*_FOOT_JOINT": 0.00,
+        },  # Default joint positions
+        joint_vel={".*": 0.0},
     ),
     soft_joint_pos_limit_factor=0.95,  # Safety factor for joint limits
     actuators={
-        "legs_and_wheels": ArticCfg.ActuatorCfg(  # Customize groups as per your robot
+        "legs_and_wheels": ArticulationCfg.ActuatorCfg(  # Customize groups as per your robot
             joint_names_expr=[".*_HIP_JOINT", ".*_THIGH_JOINT", ".*_CALF_JOINT", ".*_WHEEL_JOINT"],  # Regex for joint names
             effort_limit=20.0,  # Torque limit (adjust based on hardware)
             velocity_limit=100.0,  # Speed limit
