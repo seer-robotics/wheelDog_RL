8ab42c7c53b0025682ae55cef5b4fbd906c4c060
Renda Yang <rendayang2002@163.com>
2026-01-19 10:39:24 +0800

    Task ID update.

--- source/wheelDog_RL/wheelDog_RL/tasks/manager_based/wheeldog_rl/__init__.py
+++ source/wheelDog_RL/wheelDog_RL/tasks/manager_based/wheeldog_rl/__init__.py
@@ -13,7 +13,7 @@ from . import agents
 
 
 gym.register(
-    id="Template-Wheeldog-Rl-v0",
+    id="Wheeldog-Rl-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
