5279cab423c693c8b39c86772e3a877695ee1d55
Renda Yang <rendayang2002@163.com>
2026-01-21 16:31:55 +0800

    Contact forces observations.

--- source/wheelDog_RL/wheelDog_RL/tasks/manager_based/wheeldog_rl/customObservations.py
+++ source/wheelDog_RL/wheelDog_RL/tasks/manager_based/wheeldog_rl/customObservations.py
@@ -9,19 +9,32 @@ from __future__ import annotations
 import torch
 from typing import TYPE_CHECKING
 
+from isaaclab.assets import Articulation
 from isaaclab.managers import SceneEntityCfg
 from isaaclab.sensors import ContactSensor
+from isaaclab.utils.math import quat_apply_inverse
 
 if TYPE_CHECKING:
     from isaaclab.envs import ManagerBasedRLEnv
 
 
-def net_contact_force(env: ManagerBasedRLEnv, sensor_cfg: SceneEntityCfg) -> torch.Tensor:
-    """Extract the total contact forces on specified bodies."""
+def contact_forces(env: ManagerBasedRLEnv, sensor_cfg: SceneEntityCfg) -> torch.Tensor:
+    """Extract the normal contact forces on specified bodies."""
     # Enable type-hinting.
     contact_sensor: ContactSensor = env.scene.sensors[sensor_cfg.name]
-    net_contact_forces = contact_sensor.data.net_forces_w_history
-    # Compute 3-axis contact forces.
-    net_force = torch.max(torch.norm(net_contact_forces[:, :, sensor_cfg.body_ids], dim=-1), dim=1)[0]
-    # Compute 3-axis sum, clip 0, and return.
-    return torch.sum(net_force.clip(min=0.0), dim=1)
+    robot: Articulation = env.scene["robot"]
+
+    # Normal forces in world frame.
+    net_forces_w = contact_sensor.data.net_forces_w
+    
+    # Isolate data from specified bodies.
+    wheel_ids = sensor_cfg.body_ids
+    wheel_forces_w = net_forces_w[:, wheel_ids]
+    
+    # Acquire robot base frame quarternions. 
+    base_quats_w = robot.data.root_link_quat_w
+
+    # Transform forces to robot base frame and return.
+    return quat_apply_inverse(base_quats_w.unsqueeze(1), wheel_forces_w)
+
+    
