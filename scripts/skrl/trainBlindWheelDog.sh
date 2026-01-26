bca91b914694b2d463906f151dfc595d5dc38939
Renda Yang <rendayang2002@163.com>
2026-01-26 17:58:08 +0800

    Training shell script.

---
+++ scripts/skrl/trainBlindWheelDog.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Make this file executable:
+# chmod a+x scripts/skrl/trainBlindWheelDog.sh
+
+# Run the train.py script with arguments
+python scripts/skrl/train.py \ 
+    --task=Wheeldog-Rl-v0 \ 
+    --headless \ 
+    --livestream=1 \ 
+    --enable_cameras \ 
+    --max_iterations=1e4
