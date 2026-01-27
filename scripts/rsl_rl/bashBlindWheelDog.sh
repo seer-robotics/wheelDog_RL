02e7c467639bcc8f2a57951b55317207c3a164e7
Renda Yang <rendayang2002@163.com>
2026-01-27 18:02:39 +0800

    rsl_rl obs_group key warning.
    The current codes can work because rsl_rl auto located the observation groups, but this behavior will be removed in the future.\
    This needs to be addressed in our codes later.

--- scripts/rsl_rl/bashBlindWheelDog.sh
+++ scripts/rsl_rl/bashBlindWheelDog.sh
@@ -9,3 +9,7 @@ python scripts/rsl_rl/train.py \
     --headless \
     --livestream 1 \
     --enable_cameras
+
+# /home/renda/miniforge3/envs/bin_isaaclab/lib/python3.11/site-packages/rsl_rl/utils/utils.py:245: UserWarning: The observation configuration dictionary 'obs_groups' must contain the 'policy' key. As an observation group with the name 'policy' was found, this is assumed to be the observation set. Consider adding the 'policy' key to the 'obs_groups' dictionary for clarity. This behavior will be removed in a future version.
+#   warnings.warn(
+# /home/renda/miniforge3/envs/bin_isaaclab/lib/python3.11/site-packages/rsl_rl/utils/utils.py:283: UserWarning: The observation configuration dictionary 'obs_groups' must contain the 'critic' key. As an observation group with the name 'critic' was found, this is assumed to be the observation set. Consider adding the 'critic' key to the 'obs_groups' dictionary for clarity. This behavior will be removed in a future version.
