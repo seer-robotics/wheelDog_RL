738b56462e6f9f3c184520cee72f35f0a4ad136e
Renda Yang <rendayang2002@163.com>
2026-01-27 17:59:02 +0800

    rsl_rl implementation.

--- source/wheelDog_RL/wheelDog_RL/tasks/manager_based/wheeldog_rl/__init__.py
+++ source/wheelDog_RL/wheelDog_RL/tasks/manager_based/wheeldog_rl/__init__.py
@@ -19,5 +19,6 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": f"{__name__}.robotCfg:WheelDog_BlindLocomotionEnvCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
     },
 )
