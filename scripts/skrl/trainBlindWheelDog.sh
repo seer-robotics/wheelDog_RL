085fc4826b6b8cac6bab12d83ee481f1abd15be3
Renda Yang <rendayang2002@163.com>
2026-01-27 15:17:33 +0800

    Added executable permission.
    Added executable permission to invoke training shell script.

--- scripts/skrl/trainBlindWheelDog.sh
+++ scripts/skrl/trainBlindWheelDog.sh
@@ -4,9 +4,8 @@
 # chmod a+x scripts/skrl/trainBlindWheelDog.sh
 
 # Run the train.py script with arguments
-python scripts/skrl/train.py \ 
-    --task=Wheeldog-Rl-v0 \ 
-    --headless \ 
-    --livestream=1 \ 
-    --enable_cameras \ 
-    --max_iterations=1e4
+python scripts/skrl/train.py \
+    --task Wheeldog-Rl-v0 \
+    --headless \
+    --livestream 1 \
+    --enable_cameras
