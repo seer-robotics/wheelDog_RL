871d3bf20636ef529b70d2cfb72dacfbc3e6f3e6
Renda Yang <rendayang2002@163.com>
2026-01-29 17:24:59 +0800

    Fixed livestream variable.

--- scripts/rsl_rl/bashBlindWheelDog.sh
+++ scripts/rsl_rl/bashBlindWheelDog.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Make this file executable:
-# chmod a+x scripts/rsl_rl/trainBlindWheelDog.sh
+# chmod a+x scripts/rsl_rl/bashBlindWheelDog.sh
 
 # Run the train.py script with arguments
 python scripts/rsl_rl/train.py \
     --num_envs 4096 \
     --task Wheeldog-Rl-v0 \
     --headless \
-    --livestream 0
+    --livestream 2 \
+    --enable_cameras
 
-# /home/renda/miniforge3/envs/bin_isaaclab/lib/python3.11/site-packages/rsl_rl/utils/utils.py:245: UserWarning: The observation configuration dictionary 'obs_groups' must contain the 'policy' key. As an observation group with the name 'policy' was found, this is assumed to be the observation set. Consider adding the 'policy' key to the 'obs_groups' dictionary for clarity. This behavior will be removed in a future version.
-#   warnings.warn(
-# /home/renda/miniforge3/envs/bin_isaaclab/lib/python3.11/site-packages/rsl_rl/utils/utils.py:283: UserWarning: The observation configuration dictionary 'obs_groups' must contain the 'critic' key. As an observation group with the name 'critic' was found, this is assumed to be the observation set. Consider adding the 'critic' key to the 'obs_groups' dictionary for clarity. This behavior will be removed in a future version.
+# python scripts/rsl_rl/train.py \
+#     --num_envs 4096 \
+#     --task Wheeldog-Rl-v0 \
+#     --headless \
+#     --livestream 0
