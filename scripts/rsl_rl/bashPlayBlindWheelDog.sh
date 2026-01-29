af81ef2fd494b2173d3e60d3ce4563c39725587a
Renda Yang <rendayang2002@163.com>
2026-01-29 17:25:10 +0800

    Play preview bash script.

---
+++ scripts/rsl_rl/bashPlayBlindWheelDog.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Make this file executable:
+# chmod a+x scripts/rsl_rl/bashPlayBlindWheelDog.sh
+
+# Run the train.py script with arguments
+python scripts/rsl_rl/play.py \
+    --num_envs 8 \
+    --task Wheeldog-Rl-v0-play \
+    --headless \
+    --livestream 2 \
+    --enable_cameras
