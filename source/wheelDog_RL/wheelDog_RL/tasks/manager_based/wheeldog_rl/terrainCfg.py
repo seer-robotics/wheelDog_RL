1f505a7e620cb6e5e2289ad36d03ee8626494402
Renda Yang <rendayang2002@163.com>
2026-02-04 14:06:11 +0800

    Updated terrains cfg.

--- source/wheelDog_RL/wheelDog_RL/tasks/manager_based/wheeldog_rl/terrainCfg.py
+++ source/wheelDog_RL/wheelDog_RL/tasks/manager_based/wheeldog_rl/terrainCfg.py
@@ -12,14 +12,23 @@ allTerrain_config = TerrainGeneratorCfg(
     slope_threshold=0.75,
     use_cache=False,
     sub_terrains={
+        "flat_ground": terrain_gen.MeshPlaneTerrainCfg(
+            proportion=0.1,
+        ),
         "hf_pyramid_slope": terrain_gen.HfPyramidSlopedTerrainCfg(
             proportion=0.1, slope_range=(0.0, 0.25), platform_width=2.0, border_width=0.25
         ),
         "hf_pyramid_slope_inv": terrain_gen.HfInvertedPyramidSlopedTerrainCfg(
             proportion=0.1, slope_range=(0.0, 0.25), platform_width=2.0, border_width=0.25
         ),
+        "boxes": terrain_gen.MeshRandomGridTerrainCfg(
+            proportion=0.2, grid_width=0.45, grid_height_range=(0.0, 0.06), platform_width=2.0
+        ),
+        "random_rough": terrain_gen.HfRandomUniformTerrainCfg(
+            proportion=0.2, noise_range=(0.0, 0.06), noise_step=0.01, border_width=0.25
+        ),
         "pyramid_stairs_25cm": terrain_gen.MeshPyramidStairsTerrainCfg(
-            proportion=0.1,
+            proportion=0.075,
             step_height_range=(0.0, 0.20),
             step_width=0.25,
             platform_width=3.0,
@@ -27,18 +36,15 @@ allTerrain_config = TerrainGeneratorCfg(
             holes=False,
         ),
         "pyramid_stairs_25cm_inv": terrain_gen.MeshInvertedPyramidStairsTerrainCfg(
-            proportion=0.1,
+            proportion=0.075,
             step_height_range=(0.0, 0.20),
             step_width=0.25,
             platform_width=3.0,
             border_width=1.0,
             holes=False,
         ),
-        # "flat_ground": terrain_gen.MeshPlaneTerrainCfg(
-        #     proportion=0.2,
-        # ),
         "pyramid_stairs_30cm": terrain_gen.MeshPyramidStairsTerrainCfg(
-            proportion=0.1,
+            proportion=0.075,
             step_height_range=(0.0, 0.20),
             step_width=0.30,
             platform_width=3.0,
@@ -46,18 +52,12 @@ allTerrain_config = TerrainGeneratorCfg(
             holes=False,
         ),
         "pyramid_stairs_30cm_inv": terrain_gen.MeshInvertedPyramidStairsTerrainCfg(
-            proportion=0.1,
+            proportion=0.075,
             step_height_range=(0.0, 0.20),
             step_width=0.30,
             platform_width=3.0,
             border_width=1.0,
             holes=False,
-        ),
-        "boxes": terrain_gen.MeshRandomGridTerrainCfg(
-            proportion=0.2, grid_width=0.45, grid_height_range=(0.0, 0.06), platform_width=2.0
-        ),
-        "random_rough": terrain_gen.HfRandomUniformTerrainCfg(
-            proportion=0.2, noise_range=(0.0, 0.06), noise_step=0.01, border_width=0.25
         ),
     },
 )
