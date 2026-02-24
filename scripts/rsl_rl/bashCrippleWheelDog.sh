988b924571e9a461048f98d3eb6ef7aa1d7c8176
Renda Yang <rendayang2002@163.com>
2026-02-24 15:36:50 +0800

    Train cripple dog bash script.

---
+++ scripts/rsl_rl/bashCrippleWheelDog.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Make this file executable:
+# chmod a+x scripts/rsl_rl/bashBlindWheelDog.sh
+
+# Run the train.py script with arguments
+# python scripts/rsl_rl/train.py \
+#     --num_envs 512 \
+#     --task Crippledog-Rl-v0 \
+#     --headless \
+#     --livestream 2 \
+#     --enable_cameras
+
+HYDRA_FULL_ERROR=1 python scripts/rsl_rl/train.py \
+    --num_envs 4096 \
+    --task Crippledog-Rl-v0 \
+    --headless \
+    --livestream 0
