6b0c5084a20a9c1ea65f12b642a29ac6a07828ec
Renda Yang <rendayang2002@163.com>
2026-02-26 15:32:20 +0800

    Crippled action cfg.

--- source/wheelDog_RL/wheelDog_RL/tasks/manager_based/wheeldog_rl/actionSpaceCfg.py
+++ source/wheelDog_RL/wheelDog_RL/tasks/manager_based/wheeldog_rl/actionSpaceCfg.py
@@ -99,3 +99,92 @@ class ActionsCfg:
         use_default_offset=True,
         clip={".*": (-160.0, 160.0)},
     )
+
+
+@configclass
+class CrippledActionsCfg:
+    """Action specifications for the MDP."""
+    abdomen_joint_pos = mdp.JointPositionActionCfg(
+        asset_name="robot",
+        joint_names=[
+            "FBL_ABAD_JOINT",
+            "FAR_ABAD_JOINT",
+            "RBL_ABAD_JOINT",
+            "RAR_ABAD_JOINT",
+        ],
+        use_default_offset=False,
+        offset=0.0,
+        scale=0.4,
+        preserve_order=True,
+        clip={".*": (-0.49, 0.49)},
+    )
+    front_hip_joint_pos = mdp.JointPositionActionCfg(
+        asset_name="robot",
+        joint_names=[
+            "FBL_HIP_JOINT",
+            "FAR_HIP_JOINT",
+        ],
+        use_default_offset=False,
+        offset=1.0,
+        scale=0.6,
+        preserve_order=True,
+        clip={".*": (-1.15, 2.97)},
+    )
+    rear_hip_joint_pos = mdp.JointPositionActionCfg(
+        asset_name="robot",
+        joint_names=[
+            "RBL_HIP_JOINT",
+            "RAR_HIP_JOINT",
+        ],
+        use_default_offset=False,
+        offset=0.3,
+        scale=0.6,
+        preserve_order=True,
+        clip={".*": (-1.15, 2.97)},
+    )
+    front_knee_joint_pos = mdp.JointPositionActionCfg(
+        asset_name="robot",
+        joint_names=[
+            "FBL_KNEE_JOINT",
+            "FAR_KNEE_JOINT",
+        ],
+        use_default_offset=False,
+        offset=-2.0,
+        scale=0.6,
+        preserve_order=True,
+        clip={".*": (-2.72, -0.60)},
+    )
+    rear_knee_joint_pos = mdp.JointPositionActionCfg(
+        asset_name="robot",
+        joint_names=[
+            "RBL_KNEE_JOINT",
+            "RAR_KNEE_JOINT",
+        ],
+        use_default_offset=False,
+        offset=-1.2,
+        scale=0.5,
+        preserve_order=True,
+        clip={".*": (-2.72, -0.60)},
+    )
+    front_wheel_joint_pos = mdp.JointPositionActionCfg(
+        asset_name="robot",
+        joint_names=[
+            "FBL_FOOT_JOINT",
+            "FAR_FOOT_JOINT",
+        ],
+        use_default_offset=False,
+        offset=0.0,
+        scale=1.0,
+        preserve_order=True,
+    )
+    rear_wheel_joint_vel = mdp.JointVelocityActionCfg(
+        asset_name="robot",
+        joint_names=[
+            "RBL_FOOT_JOINT",
+            "RAR_FOOT_JOINT",
+        ],
+        scale=WHEEL_ACTION_SCALE,
+        preserve_order=True,
+        use_default_offset=True,
+        clip={".*": (-160.0, 160.0)},
+    )
